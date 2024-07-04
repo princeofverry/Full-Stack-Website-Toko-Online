e42227c4f3ba58a2c88fcc56987e6aad9f2db411
princeofverry <verrykurniawan@students.undip.ac.id>
2024-07-04 19:34:55 +0700

    add: backend db & toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import { ModalProvider } from "@/provider/modal-provider";
+import { ModalProvider } from "@/providers/modal-provider";
+import { ToasterProvider } from "@/providers/toast-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -20,6 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ToasterProvider />
           <ModalProvider />
           {children}
         </body>
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,10 +1,57 @@
 "use client";
 
+import * as z from "zod";
+
 import { useStoreModal } from "@/hooks/use-store-modal";
 import Modal from "../ui/modal";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 
 export const Storemodal = () => {
+  const [loading, setloading] = useState(false);
+
   const storeModal = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    // TODO: buat toko
+    try {
+      setloading(true);
+
+      const response = await axios.post("/api/stores", values);
+
+      console.log(response.data);
+      toast.success("berhasil membuat toko");
+    } catch (error) {
+      toast.error("Gagal membuat Toko");
+    } finally {
+      setloading(false);
+    }
+  };
+
   return (
     <Modal
       title="Buat Store"
@@ -12,7 +59,43 @@ export const Storemodal = () => {
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
-      store form
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Nama Toko"
+                        {...field}
+                        disabled={loading}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button
+                  variant="outline"
+                  disabled={loading}
+                  onClick={storeModal.onClose}
+                >
+                  Cancel
+                </Button>
+                <Button type="submit" disabled={loading}>
+                  Continue
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
     </Modal>
   );
 };
